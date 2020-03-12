96d7127f51a4d4c370c49b97a170adc3f14cda10
mrsunay <mrsunay@gmail.com>
2020-03-12 09:57:36 -0500

    Solution

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
-https://github.com/mrsunay/counting-valleys.git
+def countingValleys(n, steps):
+    seaLevel = valley = 0
+
+    for step in steps:
+        if step == 'U':
+            seaLevel += 1
+        else:
+            seaLevel -= 1
+        
+        if step == 'U' and seaLevel == 0:
+            valley += 1
+    
+    return valley    
